895e45cd548b5f63a512d7e93bdf98e3de84e9f4
Luk4x <luk4xm4ci3l@gmail.com>
2022-06-14 13:09:19 -0300

    get, post, put created

--- index.js
+++ index.js
@@ -4,11 +4,104 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`㊗️ - server started in port: ${port}.`);
+// users registered
+const usersList = [];
+
+// middleware's
+const showMethodNUrl = (req, res, next) => {
+    console.log(`${req.method}: ${req.url}`);
+
+    next();
+};
+app.use(showMethodNUrl);
+
+const checkIdExistence = (req, res, next) => {
+    // finding user by id
+    const userId = req.params.id;
+    const userIndex = usersList.findIndex(user => user.id === userId);
+
+    // return error if not found
+    if (userIndex < 0) {
+        return res.status(404).json({ error: 'User Not Found' });
+    }
+
+    // incorporating the user index in request
+    req.userIndex = userIndex;
+
+    next();
+};
+
+// verify if client is trying to change or add id information
+const verifyClientData = (req, res, next) => {
+    const { id } = req.body;
+
+    if (id) {
+        return res.status(401).json({ error: 'Unable to change id information' });
+    }
+
+    next();
+};
+
+// routes
+
+// view all users
+app.get('/users', (req, res) => {
+    try {
+        return res.json(usersList);
+    } catch (err) {
+        console.error(`Something happened: ${err.message}`);
+        return res.status(500).json({ error: err.message });
+    }
 });
 
-app.get('/yo', (req, res) => {
-    const yo = 'Yo!';
-    return res.json({ yo });
+// create a user
+app.post('/users', verifyClientData, (req, res) => {
+    /* request(body) pattern:
+        {
+            "name": "Leone",
+            "age": "22"
+        }
+    */
+
+    // receiving data
+    const { name, age } = req.body;
+    const registeredUser = { name, age, id: uuid.v4() };
+
+    // verifying data received
+    for (userData in registeredUser) {
+        if (!registeredUser[userData]) return res.status(400).json({ error: 'Missing Data' });
+    }
+
+    // trying to register user
+    try {
+        usersList.push(registeredUser);
+        return res.status(201).json(registeredUser);
+    } catch (err) {
+        console.error(`Something happened: ${err.message}`);
+        return res.status(500).json({ error: err.message });
+    }
+});
+
+// updating an user (by id)
+app.put('/users/:id', checkIdExistence, verifyClientData, (req, res) => {
+    // receiving user data
+    const { name, age } = req.body;
+    const newData = { name, age };
+
+    try {
+        // function to update one or more data from an user
+        for (let userData in usersList[req.userIndex]) {
+            for (data in newData) {
+                if (newData[data] && data === userData) usersList[req.userIndex][data] = newData[data];
+            }
+        }
+        return res.json({ updatedUser: usersList[req.userIndex] });
+    } catch (err) {
+        console.error(`Something happened: ${err.message}`);
+        return res.status(500).json({ error: err.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`㊗️ - server started in port: ${port}.`);
 });
